f423059ba1b4264c239fc8282383803e9d9151f8
riyatdul <alriyatdul@gmail.com>
2024-10-02 16:35:10 +0700

    add/project-5-form-validation

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ import { createApp } from 'vue'
 // import App from './1-todo-list/App.vue'
 // import App from './2-random-quote-generator/App.vue'
 // import App from './3-calculator/App.vue'
-import App from './4-accordion-list/App.vue'
+// import App from './4-accordion-list/App.vue'
+import App from './5-form-validator/App.vue'
 
 
 createApp(App).mount('#app')
